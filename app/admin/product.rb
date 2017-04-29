f155d8a02fdfdb75441717a4ec14a10efa841894
JakubKopysDev <jakub.kopys@o2.pl>
2017-04-29 02:29:43 +0200

    Admin Products and Users pages

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,15 +1,62 @@
 ActiveAdmin.register Product do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :name, :description, :price, :image
 
+  config.per_page = 4
+  config.sort_order = 'desc'
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :description
+    column :price
+    column :image do |product|
+      image_tag product.image(:medium), class: 'admin-thumbnail'
+    end
+    column :created_at
+    actions
+  end
+
+  show do |product|
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :image do |prodcut|
+        image_tag product.image(:medium), class: 'admin-thumbnail'
+      end
+      row :image_urls do |product|
+        columns do
+          column do
+            link_to "Original: #{product.image}", product.image.url
+          end
+          column do
+            link_to "Medium: #{product.image(:medium)}", product.image(:medium)
+          end
+        end
+      end
+      row :image_file_name
+      row :image_content_type
+      row :image_file_size
+      row :image_updated_at
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.inputs 'Product data' do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :image
+    end
+    f.actions
+  end
+
+  filter :id
+  filter :name
+  filter :description
+  filter :price
+  filter :created_at
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,15 +1,26 @@
 ActiveAdmin.register User do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :email, :name, :password, :password_confirmation
 
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :email
+    actions
+  end
+
+  form do |f|
+    f.inputs 'User info' do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+  filter :id
+  filter :name
+  filter :email
+  filter :created_at
 end
